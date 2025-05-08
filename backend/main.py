517897127478c0a0da63ef009768b1186032ad2b
Ana Villagrán <96851076+avillagran801@users.noreply.github.com>
2025-05-08 18:51:03 -0400

    Setup new dataset
    The new dataset comes from
    
    https://www.kaggle.com/datasets/alanvourch/tmdb-movies-daily-updates

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import ORJSONResponse
 import pandas as pd
 
 app = FastAPI()
@@ -8,18 +9,23 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/data")
-def get_data():
-    # Replace with your own CSV/ZIP file
-    df = pd.read_csv("revenues_per_day.zip")
+try:
+  movies_df = pd.read_csv("TMDB_all_movies.zip")
+  movies_df = movies_df.replace([float("inf"), float("-inf")], None)  # Replace infinities with None
+  print("CSV loaded:", movies_df.shape)
 
-    # Clean NaN and infinite values in the DataFrame
-    df = df.replace([float("inf"), float("-inf")], None)  # Replace infinities with None
-    df = df.dropna()  # Drop rows with NaN values (optional)
-    
-    # Return as JSON
-    return df.to_dict(orient="records")
+except Exception as e:
+  print("Failed to load CSV:", str(e))
+  movies_dataframe = pd.DataFrame()
+
+
+@app.get("/data", response_class=ORJSONResponse)
+def get_data():
+    if movies_df.empty:
+        raise HTTPException(status_code=500, detail="Data not loaded.")
+    return movies_df.head(200).to_dict(orient="records")
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,80 +1,57 @@
 import React from "react";
 
-type DataPoint = {
-  date: string;
-  distributor: string;
-  id: string;
-  revenue: number;
-  theaters: number;
+type Movie = {
+  id: number;
   title: string;
+  vote_average: number;
+  vote_count: number;
+  status: string;
+  release_date: Date;
+  revenue: number;
+  runtime: number;
+  budget: number;
+  imdb_id: string;
 }
 
+
 export default function Home() {
-  const [data, setData] = React.useState<DataPoint[]>([]);
+  const [loading, setLoading] = React.useState(true);
+  const [data, setData] = React.useState<Movie[]>([]);
 
   React.useEffect(() => {
     fetch("http://localhost:8000/data")
-      .then(res => res.json())
-      .then(setData);
+      .then(async res => {
+        const json = await res.json();
+        console.log("Fetched data:", json);
+        setData(json);
+        setLoading(false);
+      })
+      .catch(err => console.error("Fetch error:", err));
   }, []);
 
-  const test = data[0];
-  console.log(test);
+  if(loading){
+    return(
+      <>
+        Cargando...
+      </>
+    )
+  }
 
   return (
     <>
-      <div style={{ 
-        padding: "2rem",
-        display: "flex",
-        flexDirection: "column",
-        gap: "1.4rem"
-      }}>
-        <h1>
-          MI PRIMERA PÁGINA WEB
-        </h1>
-        <p>
-          HOLA ESTA ES MI PAGNIA WEB LOS QUIERO MUCHO
-        </p>
-        <h2>
-          NOELIA
-        </h2>
-        <p>
-          ESTOY HACIENDO MI PAGINA WEB QUE EMOCION
-        </p>
-        <p>
-          <b>
-            BUENO Y AHORA QUE HAGO
-          </b>
-        </p>
-        <p>
-          <b>
-            SI
-          </b>
-        </p>
-        <p>
-          © Todos los derechos reservados 2019
-        </p>
-      </div>
       <div style={{ padding: "2rem"}}>
-        PRIMER DATO:
-        <p>
-          Fecha: {test.date}
-        </p>
-        <p>
-          Distribuidor: {test.distributor}
-        </p>
-        <p>
-          Id: {test.id}
-        </p>
-        <p>
-          Ganancia: {test.revenue}
-        </p>
-        <p>
-          Teatros: {test.theaters}
-        </p>
-        <p>
-          Título: {test.title}
-        </p>
+        <div>
+          <h1>
+            PRIMERAS 200 PELÍCULAS DEL DATAFRAME 
+          </h1>
+        </div>
+        <div>
+          {data && data.map((movie) => (
+            <div key={movie.id}>
+              {movie.title}
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
